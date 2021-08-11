959d54d867b17d9cdb6c84dc0ed5ee1351618aca
mmr-ashiq <ashik156rahman@gmail.com>
2021-08-11 17:24:14 +0600

    vector.cpp file added

--- 01. Arrays & Vector/Implemented in C++/pairs-twoSum.cpp
+++ 01. Arrays & Vector/Implemented in C++/pairs-twoSum.cpp
@@ -22,9 +22,9 @@ vector<int> pairSum(vector<int> arr, int Sum) {
 	unordered_set<int> s;
 	vector<int> result;
 
-	for(int i=0; i<arr.size(); i++){
+	for (int i = 0; i < arr.size(); i++) {
 		int x = Sum - arr[i];
-		if(s.find(x)!=s.end()){
+		if (s.find(x) != s.end()) {
 			result.push_back(x);
 			result.push_back(arr[i]);
 			return result;
---
+++ STL/01. vector/vector.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int32_t main()
+{
+	vector<int> v = {10, 8, 3, 15, 20, 9, 30, 5};
+
+	/*
+		an iterator gives you address of an element in a container.
+	*/
+	// vector<int> :: iterator i = v.begin();
+
+	auto i = v.begin();
+	cout << (*i) << endl;
+
+	i++;
+	cout << *i << endl;
+
+	i = next(i);
+	cout << *i << endl;
+
+	i = next(i, 2);
+	cout << *i << endl;
+
+	i = prev(i);
+	cout << *i << endl;
+
+	i = prev(i, 2);
+	cout << *i << endl;
+
+	// advance(iterator, nth) function pass the iterator nth position ahead
+	advance(i, 3);
+	cout << *i << endl;
+	
+	return 0;
+}
